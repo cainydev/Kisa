61e9addfc06831e5eba1c1d9eb980b0c2a806de7
John Wagner <info@techbra.in>
2022-08-23 18:07:55 +0200

    Added Edit Button to DeliveryListBagLayout.php

--- app/Orchid/Layouts/Delivery/DeliveryListBagLayout.php
+++ app/Orchid/Layouts/Delivery/DeliveryListBagLayout.php
@@ -2,7 +2,9 @@
 
 namespace App\Orchid\Layouts\Delivery;
 
+use App\Orchid\Fields\Group;
 use Orchid\Screen\Actions\Button;
+use Orchid\Screen\Actions\Link;
 use Orchid\Screen\Layouts\Table;
 use Orchid\Screen\TD;
 use Orchid\Support\Facades\Layout;
@@ -18,21 +20,29 @@ class DeliveryListBagLayout extends Table
             TD::make('id', 'ID'),
             TD::make('herb.name', 'Kraut'),
             TD::make('size', 'Gebinde')
-            ->render(function($bag) {
-                return sprintf('%.1fkg ', ($bag->size / 1000.0));
-            }),
+                ->render(function ($bag) {
+                    return sprintf('%.1fkg ', ($bag->size / 1000.0));
+                }),
             TD::make('charge', 'Charge'),
             TD::make('bio', 'Bio?')
-            ->render(function ($bag) {
-                return view('partials/boolean', ['value' => $bag->bio]);
-            }),
-            TD::make('delete', 'Löschen')
-            ->render(function ($bag) {
-                return Button::make()
-                    ->class('btn btn-danger p-2')
-                    ->method('deleteBag', ['bag' => $bag->id])
-                    ->icon('trash');
-            })
+                ->render(function ($bag) {
+                    return view('partials/boolean', ['value' => $bag->bio]);
+                }),
+            TD::make('delete', 'Aktionen')
+                ->align(TD::ALIGN_RIGHT)
+                ->render(function ($bag) {
+                    return Group::make([
+                        Button::make()
+                            ->class('btn btn-danger p-2')
+                            ->method('deleteBag', ['bag' => $bag->id])
+                            ->icon('trash'),
+                        Link::make()
+                            ->class('btn btn-primary')
+                            ->icon('pencil')
+                            ->class('btn btn-primary p-2')
+                            ->route('platform.bags.edit', $bag),
+                    ]);
+                })
         ];
     }
 }
