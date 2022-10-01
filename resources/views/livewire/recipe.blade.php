b8783a0df7685748335461c796e4125ce97481f8
cainytheslave <wajo432@gmail.com>
2022-10-01 16:06:06 +0100

    Shrinkened the space on the recipe page to fit more stuff

--- resources/views/livewire/recipe.blade.php
+++ resources/views/livewire/recipe.blade.php
@@ -1,7 +1,7 @@
 <div class="flex flex-col mt-8 bg-white rounded">
 
     {{-- Settings of this bottle --}}
-    <div class="grid gap-4 lg:grid-cols-2">
+    <div class="grid gap-2 lg:grid-cols-2">
         <div>
             <p class="mb-2 font-bold tracking-wider uppercase">Abfüllung von</p>
             <x-recipe-title :position="$position" />
@@ -9,10 +9,15 @@
             <div class="p-2 mt-12 border rounded-md w-fit">
                 <span class="flex flex-row items-center shadow h-9 w-min">
                     <div class="h-full overflow-hidden rounded-l-md bg-billbee w-9">
-                        <img wire:loading.remove class="h-full " src="{{ asset('images/billbee.png') }}" alt="">
-                        <x-icons.icon-loading wire:loading.flex class="fill-white" />
+                        <img wire:loading.remove
+                             class="h-full "
+                             src="{{ asset('images/billbee.png') }}"
+                             alt="">
+                        <x-icons.icon-loading wire:loading.flex
+                                              class="fill-white" />
                     </div>
-                    <button class="h-full px-3 font-semibold bg-gray-200 rounded-r-md" wire:click="refreshStock">Aktualisieren</button>
+                    <button class="h-full px-3 font-semibold bg-gray-200 rounded-r-md"
+                            wire:click="refreshStock">Aktualisieren</button>
                 </span>
                 <p class="mt-2 text-lg">Aktueller Bestand in Billbee: {{ $position->variant->stock }} </p>
             </div>
@@ -23,9 +28,13 @@
             {{-- Set charge manually --}}
             <p class="form-label">Charge</p>
             <div class="flex justify-start">
-                <input wire:model="newCharge" type="text" class="mr-3 rounded form-control" />
-                <button wire:click="setCharge" class="mr-3 btn btn-success whitespace-nowrap">Speichern</button>
-                <button wire:click="generateCharge" class="btn btn-primary whitespace-nowrap">Auto-Charge</button>
+                <input wire:model="newCharge"
+                       type="text"
+                       class="mr-3 rounded form-control" />
+                <button wire:click="setCharge"
+                        class="mr-3 btn btn-success whitespace-nowrap">Speichern</button>
+                <button wire:click="generateCharge"
+                        class="btn btn-primary whitespace-nowrap">Auto-Charge</button>
             </div>
 
             {{-- Upload to Billbee --}}
@@ -33,17 +42,21 @@
                 <p class="form-label">Upload zu Billbee</p>
                 @if(!$position->uploaded)
                 @if($position->hasAllBags())
-                <button wire:click="uploadToBillbee" class="mr-3 btn btn-success whitespace-nowrap">
-                    <span wire:loading.remove >Produkt einlagern in Billbee</span>
-                    <x-icons.icon-loading wire:loading.flex class="w-8 h-8 mx-auto fill-white" />
+                <button wire:click="uploadToBillbee"
+                        class="mr-3 btn btn-success whitespace-nowrap">
+                    <span wire:loading.remove>Produkt einlagern in Billbee</span>
+                    <x-icons.icon-loading wire:loading.flex
+                                          class="w-8 h-8 mx-auto fill-white" />
                 </button>
                 @else
-                <div class="alert alert-warning" role="alert">
+                <div class="alert alert-warning"
+                     role="alert">
                     <p>Fülle zuerst das ganze Produkt ab bevor du es in Billbee einlagern kannst.</p>
                 </div>
                 @endif
                 @else
-                <div class="alert alert-success" role="alert">
+                <div class="alert alert-success"
+                     role="alert">
                     <p>Diese Abfüllung wurde bereits in Billbee eingelagert.</p>
                 </div>
                 @endif
@@ -54,19 +67,22 @@
 
         <div class="col-span-full">
             @if (session()->has('success'))
-            <div class="my-3 alert alert-success" role="alert">
+            <div class="alert alert-success"
+                 role="alert">
                 {{ session('success') }}
             </div>
             @endif
 
             @if (session()->has('warning'))
-            <div class="my-3 alert alert-warning" role="alert">
+            <div class="alert alert-warning"
+                 role="alert">
                 {{ session('warning') }}
             </div>
             @endif
 
             @if (session()->has('error'))
-            <div class="my-3 alert alert-danger" role="alert">
+            <div class="alert alert-danger"
+                 role="alert">
                 {{ session('error') }}
             </div>
             @endif
@@ -74,17 +90,25 @@
 
 
         {{-- Recipe itself --}}
-        <p class="mt-2 mb-2 font-bold tracking-wider uppercase">Zutaten</p>
-        <div class="accordion col-span-full" id="wrapper">
+        <p class="font-bold tracking-wider uppercase">Zutaten</p>
+        <div class="accordion col-span-full"
+             id="wrapper">
 
             @php
             $show = false;
             @endphp
 
             @forelse($position->variant->product->herbs as $herb)
-            <div class="accordion-item" x-data>
-                <h2 class="accordion-header" id="heading{{ $loop->index }}">
-                    <button class="flex justify-between accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse{{ $loop->index }}" aria-expanded="false" aria-controls="collapse{{ $loop->index }}">
+            <div class="accordion-item"
+                 x-data>
+                <h2 class="accordion-header"
+                    id="heading{{ $loop->index }}">
+                    <button class="flex justify-between accordion-button"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#collapse{{ $loop->index }}"
+                            aria-expanded="false"
+                            aria-controls="collapse{{ $loop->index }}">
                         {{ $herb->name }}
                         @if($position->hasBagFor($herb))
                         <x-icons.icon-checkmark-solid class="w-8 h-8 p-1 ml-4 bg-white rounded-full fill-green-600" />
@@ -102,11 +126,15 @@
                 }
                 @endphp
 
-                <div id="collapse{{ $loop->index }}" class="accordion-collapse collapse {{ $openTab ? 'show' : '' }}" aria-labelledby="heading{{ $loop->index }}" data-bs-parent="#wrapper">
+                <div id="collapse{{ $loop->index }}"
+                     class="accordion-collapse collapse {{ $openTab ? 'show' : '' }}"
+                     aria-labelledby="heading{{ $loop->index }}"
+                     data-bs-parent="#wrapper">
 
                     {{-- Akkordeon Body --}}
                     <div class="accordion-body">
-                        <p>{{ $herb->name }} kommt mit {{ sprintf('%.1f%%', $herb->pivot->percentage) }} in dieser Mischung
+                        <p>{{ $herb->name }} kommt mit {{ sprintf('%.1f%%', $herb->pivot->percentage) }} in dieser
+                            Mischung
                             vor. Bitte wähle ein Gebinde aus, welches du zum Abfüllen verwenden willst.</p>
 
                         @php
@@ -117,11 +145,13 @@
                         {{-- Verfügbare Säcke --}}
                         <div class="grid gap-6 mt-4 md:grid-cols-2 xl:grid-cols-3">
                             @forelse($herb->bags->filter(fn($b) =>
-                            ($b->getCurrentWithTrashed() >= $gesamt || $position->ingredients->where('bag_id', $b->id)->count() == 1)
+                            ($b->getCurrentWithTrashed() >= $gesamt || $position->ingredients->where('bag_id',
+                            $b->id)->count() == 1)
                             )->sortBy('bestbefore') as
                             $bag)
                             {{-- Sack Card --}}
-                            <div class="flex flex-col max-w-lg ring-green-600 rounded {{ $position->isBagFor($bag, $herb) ? 'ring-4' : 'ring-0' }}" wire:key="bag{{ $bag->id }}">
+                            <div class="flex flex-col max-w-lg ring-green-600 rounded {{ $position->isBagFor($bag, $herb) ? 'ring-4' : 'ring-0' }}"
+                                 wire:key="bag{{ $bag->id }}">
                                 <div class="flex items-center justify-between w-full p-3 bg-gray-100">
                                     <p>{{ $bag->specification }}</p>
                                     @if($bag->bio)
@@ -131,30 +161,38 @@
                                     @endif
 
                                     @if($position->isBagFor($bag, $herb))
-                                    <button title="Nicht mehr auswählen" class="btn btn-danger" wire:click="removeBag({{ $bag->id }})">
+                                    <button title="Nicht mehr auswählen"
+                                            class="btn btn-danger"
+                                            wire:click="removeBag({{ $bag->id }})">
                                         <x-icons.icon-x class="stroke-white" />
                                     </button>
                                     @else
-                                    <button title="Auswählen" class="btn btn-success" wire:click="setBag({{ $bag->id }})">
+                                    <button title="Auswählen"
+                                            class="btn btn-success"
+                                            wire:click="setBag({{ $bag->id }})">
                                         <x-icons.icon-checkmark class="w-6 h-6 fill-white" />
                                     </button>
                                     @endif
                                 </div>
                                 <div class="p-3">
                                     <p>Haltbar bis {{ $bag->bestbefore->diffForHumans() }}</p>
-                                    <p class="text-xs text-red-500">@if($bag->bestbefore < now()->addMonths(3)) Läuft bald
+                                    <p class="text-xs text-red-500">@if($bag->bestbefore < now()->addMonths(3)) Läuft
+                                            bald
                                             ab! @endif</p>
                                 </div>
                                 <div class="flex flex-col items-stretch justify-between p-3">
                                     <p class="mb-2">Charge: {{ $bag->charge }}</p>
-                                    <p class="text-sm">Füllstand: {{ $bag->getCurrentWithTrashed() }}g / {{ $bag->size }}g</p>
+                                    <p class="text-sm">Füllstand: {{ $bag->getCurrentWithTrashed() }}g / {{ $bag->size
+                                        }}g</p>
                                     <span class="w-full mt-1 border rounded">
-                                        <span class="block h-3" style="width:{{ ($bag->getCurrentWithTrashed() / $bag->size) * 100 }}%; background: hsl({{ ($bag->getCurrentWithTrashed() / $bag->size)*120 }}, 80%, 40%)"></span>
+                                        <span class="block h-3"
+                                              style="width:{{ ($bag->getCurrentWithTrashed() / $bag->size) * 100 }}%; background: hsl({{ ($bag->getCurrentWithTrashed() / $bag->size)*120 }}, 80%, 40%)"></span>
                                     </span>
                                 </div>
                             </div>
                             @empty
-                            <p class="mt-4 col-span-full alert alert-danger">Es sind momentan keine Säcke {{ $herb->name }}
+                            <p class="mt-4 col-span-full alert alert-danger">Es sind momentan keine Säcke {{ $herb->name
+                                }}
                                 gelagert die noch die gebrauchte Menge von {{ $gesamt }}g haben.</p>
                             @endforelse
                         </div>
@@ -164,7 +202,8 @@
             </div>
             @empty
             <div class="mt-2">
-                <p><span class="font-semibold text-red-500">Achtung:</span> Für dieses Produkt wurde noch kein Rezept hinterlegt.</p>
+                <p><span class="font-semibold text-red-500">Achtung:</span> Für dieses Produkt wurde noch kein Rezept
+                    hinterlegt.</p>
                 <a href="{{ route('platform.products.edit', $position->variant->product) }}">
                     <x-button class="mt-2 font-semibold">Produkt bearbeiten</x-button>
                 </a>
