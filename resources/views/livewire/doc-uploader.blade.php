ded091cedf90b8974e04ebf6028872c1886ba803
John Wagner <info@techbra.in>
2022-10-08 13:39:48 +0200

    Added close button

--- resources/views/livewire/doc-uploader.blade.php
+++ resources/views/livewire/doc-uploader.blade.php
@@ -26,7 +26,13 @@
         <div x-data="{open: false, url: '{{ $entity->getFirstMediaUrl($collection, 'big'); }}'}" x-on:click="open = true">
             <img src="{{ $entity->getFirstMediaUrl($collection, 'small'); }}" alt="Vorschau {{ $title }}">
             <span x-show="open" x-transition class="fixed inset-0">
-                <img x-bind:src="open ? url : ''" class="fixed w-full h-full" alt="{{ $title }} in Groß">
+                <img x-bind:src="open ? url : ''" class="fixed h-full" alt="{{ $title }} in Groß">
+                <button class="absolute btn btn-lg btn-info top-3 right-3" x-on:click="open = false">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-square" viewBox="0 0 16 16">
+                        <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                    </svg>
+                </button>
             </span>
         </div>
         @else
