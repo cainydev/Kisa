839e7338a5a6b68a4143ee0ca3230cf6a4fd929d
John Wagner <info@techbra.in>
2022-10-08 15:18:20 +0200

    Finished file upload

--- app/Http/Livewire/DocUploader.php
+++ app/Http/Livewire/DocUploader.php
@@ -22,12 +22,26 @@ class DocUploader extends Component
         $this->validate();
 
         if($this->entity != null) {
-            $this->entity->addMedia($this->document->getRealPath())->toMediaCollection($this->collection);
+            $this->entity
+            ->addMedia($this->document->getRealPath())
+            ->withCustomProperties(['name' => $this->document->getClientOriginalName()])
+            ->toMediaCollection($this->collection);
         }
+    }
+
+    public function download(){
+        $media = $this->entity->getFirstMedia($this->collection);
+        $name = $media->hasCustomProperty('name') ? $media->getCustomProperty('name') : $this->title;
+        return response()->download($media->getPath(), $name);
+    }
+
+    public function delete(){
+        $this->entity->clearMediaCollection($this->collection);
     }
 
     public function render()
     {
+        $this->entity = ($this->entity::class)::find($this->entity->id);
         return view('livewire.doc-uploader');
     }
 }
--- resources/views/livewire/doc-uploader.blade.php
+++ resources/views/livewire/doc-uploader.blade.php
@@ -3,44 +3,39 @@
 <div class="flex flex-col border rounded">
     <div class="flex items-center justify-between px-3 py-2 border-b shadow-inner wrap">
         <label class="mb-0 form-label">{{ $title }}</label>
+        @if($entity->hasMedia($collection))
         <span>
-            <button class="btn btn-info" title="Download">
+            <button class="btn btn-info" title="Download" wire:click="download">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
                     <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z" />
                     <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z" />
                 </svg>
             </button>
-            <button class="btn btn-danger" title="Löschen">
+            <button class="btn btn-danger" title="Löschen" wire:click="delete">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                     <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
                     <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
                 </svg>
             </button>
         </span>
+        @endif
     </div>
 
     <div class="px-3 py-2">
         @if($entity->hasMedia($collection))
 
-        @if($entity->getFirstMedia($collection)->hasGeneratedConversion('thumb'))
-        <div x-data="{open: false, url: '{{ $entity->getFirstMediaUrl($collection, 'big'); }}'}" x-on:click="open = true">
-            <img src="{{ $entity->getFirstMediaUrl($collection, 'small'); }}" alt="Vorschau {{ $title }}">
-            <span x-show="open" x-transition class="fixed inset-0">
-                <img x-bind:src="open ? url : ''" class="fixed h-full" alt="{{ $title }} in Groß">
-                <button class="absolute btn btn-lg btn-info top-3 right-3" x-on:click="open = false">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-square" viewBox="0 0 16 16">
-                        <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
-                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-                    </svg>
-                </button>
-            </span>
-        </div>
+        @if($entity->getFirstMedia($collection)->hasGeneratedConversion('small'))
+        <img src="{{ $entity->getFirstMediaUrl($collection, 'small'); }}" alt="Vorschau {{ $title }}">
         @else
         <p>Es konnte keine Vorschau für die Datei erstellt werden.</p>
         @endif
 
         @else
-        <input type="file" wire:model="document" class="p-2 form-control" />
+        <button class="relative w-full btn btn-success">
+            <span wire:loading.remove>{{ $title }} hochladen</span>
+            <x-icons.icon-loading wire:loading.flex class="w-8 h-8 mx-auto fill-white" />
+            <input class="absolute inset-0 opacity-0 cursor-pointer" type="file" wire:model="document" />
+        </button>
         @endif
     </div>
 
