0455e44fa25ecb06bb4d98dfb9ffa8bd7f364013
cainydev <wajo432@gmail.com>
2026-01-26 18:16:55 +0100

    Fix wrong filament view

--- resources/views/livewire/edit-trashed-amount.blade.php
+++ resources/views/livewire/edit-trashed-amount.blade.php
@@ -1,8 +1,9 @@
 {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
 
 <div>
-    <x-filament-forms::field-wrapper.label>Ausschuss</x-filament-forms::field-wrapper.label>
-    <x-filament::input.wrapper label-suffix="g">
-        <x-filament::input wire:model="bag.trashed" type="number"/>
-    </x-filament::input.wrapper>
+    <x-filament-forms::field-wrapper label="Ausschuss">
+        <x-filament::input.wrapper label-suffix="g">
+            <x-filament::input wire:model="bag.trashed" type="number"/>
+        </x-filament::input.wrapper>
+    </x-filament-forms::field-wrapper>
 </div>
